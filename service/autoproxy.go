285a8b0cbb7254f716428c5f7d6350f60b6d9409
sunshineplan <sunshineplan@users.noreply.github.com>
2024-10-11 16:03:53 +0800

    Support custom autoproxy

--- service/autoproxy.go
+++ service/autoproxy.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"io"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 	"sync"
 	"time"
@@ -25,87 +25,145 @@ type Autoproxy struct {
 
 const autoproxyURL = "https://raw.githubusercontent.com/v2fly/domain-list-community/release/geolocation-!cn.txt"
 
-var last string
+var (
+	last            string
+	customAutoproxy []byte
+)
 
-var errNoUpdateAvailable = errors.New("no update available")
-
-func parseAutoproxy(c *Client) (*proxy.PerHost, error) {
-	accessLogger.Print("check autoproxy")
-	ch := make(chan *http.Response, 1)
+func fetchAutoproxy(c *Client) (string, error) {
+	accessLogger.Print("fetch autoproxy")
+	ch := make(chan []byte)
+	defer close(ch)
 	go func() {
-		if resp, err := http.Get(autoproxyURL); err == nil {
-			ch <- resp
-		} else {
+		resp, err := http.Get(autoproxyURL)
+		if err != nil {
 			errorLogger.Debug("failed to check autoproxy without using proxy", "error", err)
+			return
+		}
+		defer resp.Body.Close()
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			errorLogger.Debug("failed to check autoproxy without using proxy", "error", err)
+			return
+		}
+		select {
+		case ch <- b:
+		default:
 		}
 	}()
 	go func() {
 		if t, ok := http.DefaultTransport.(*http.Transport); ok {
 			t = t.Clone()
 			t.Proxy = http.ProxyURL(c.u)
-			if resp, err := (&http.Client{Transport: t}).Get(autoproxyURL); err == nil {
-				ch <- resp
-			} else {
+			resp, err := (&http.Client{Transport: t}).Get(autoproxyURL)
+			if err != nil {
 				errorLogger.Debug("failed to check autoproxy using proxy", "error", err)
+			}
+			defer resp.Body.Close()
+			b, err := io.ReadAll(resp.Body)
+			if err != nil {
+				errorLogger.Debug("failed to check autoproxy using proxy", "error", err)
+				return
+			}
+			select {
+			case ch <- b:
+			default:
 			}
 		}
 	}()
-	var resp *http.Response
+	var b []byte
 	select {
 	case <-time.After(time.Minute):
-		return nil, errors.New("failed to check autoproxy")
-	case resp = <-ch:
+		return "", errors.New("failed to check autoproxy")
+	case b = <-ch:
+		accessLogger.Print("autoproxy fetched")
 	}
-	defer resp.Body.Close()
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	if string(b) == last {
-		return nil, errNoUpdateAvailable
-	}
-	perHost := proxy.NewPerHost(
-		&dialerLogger{"direct", proxy.Direct},
-		&dialerLogger{"proxy", c.proxy},
-	)
-	for _, i := range txt.ReadAll(bytes.NewReader(b)) {
-		if strings.HasSuffix(i, "@cn") {
-			continue
-		}
-		i = strings.ReplaceAll(i, ":@ads", "")
-		switch {
-		case strings.HasPrefix(i, "domain:"):
-			perHost.AddZone(strings.TrimPrefix(i, "domain:"))
-		case strings.HasPrefix(i, "full:"):
-			perHost.AddHost(strings.TrimPrefix(i, "full:"))
+	return string(b), nil
+}
+
+func addPerHost(p *proxy.PerHost, s string, custom bool) *proxy.PerHost {
+	if custom {
+		p.AddFromString(s)
+	} else {
+		for _, i := range txt.ReadAll(strings.NewReader(s)) {
+			if strings.HasSuffix(i, "@cn") {
+				continue
+			}
+			i = strings.ReplaceAll(i, ":@ads", "")
+			switch {
+			case strings.HasPrefix(i, "domain:"):
+				p.AddZone(strings.TrimPrefix(i, "domain:"))
+			case strings.HasPrefix(i, "full:"):
+				p.AddHost(strings.TrimPrefix(i, "full:"))
+			}
 		}
 	}
-	last = string(b)
-	accessLogger.Print("autoproxy loaded")
-	return perHost, nil
+	return p
+}
+
+func parseAutoproxy(p *proxy.PerHost, s, custom string) *proxy.PerHost {
+	addPerHost(p, s, false)
+	addPerHost(p, custom, true)
+	return p
 }
 
 func initAutoproxy(c *Client) *proxy.PerHost {
 	accessLogger.Debug("autoproxy: " + *autoproxy)
-	p, err := parseAutoproxy(c)
+	s, err := fetchAutoproxy(c)
 	if err != nil {
 		errorLogger.Print(err)
 		return nil
 	}
+	accessLogger.Debug("custom autoproxy: " + *custom)
+	customAutoproxy, err = os.ReadFile(*custom)
+	if err != nil {
+		errorLogger.Println("failed to read custom autoproxy file:", err)
+	}
+	p := parseAutoproxy(proxy.NewPerHost(
+		&dialerLogger{"direct", proxy.Direct},
+		&dialerLogger{"proxy", c.proxy},
+	), s, string(customAutoproxy))
 	scheduler.NewScheduler().At(scheduler.AtHour(12)).Do(func(_ time.Time) {
-		c.autoproxy.Lock()
-		defer c.autoproxy.Unlock()
-		p, err := parseAutoproxy(c)
+		s, err := fetchAutoproxy(c)
 		if err != nil {
-			if err == errNoUpdateAvailable {
-				accessLogger.Println("autoproxy", err)
-			} else {
-				errorLogger.Print(err)
-			}
+			errorLogger.Print(err)
 			return
 		}
-		c.autoproxy.PerHost = p
+		if s == last {
+			accessLogger.Print("autoproxy: no update available")
+			return
+		}
+		last = s
+		c.autoproxy.Lock()
+		defer c.autoproxy.Unlock()
+		c.autoproxy.PerHost = parseAutoproxy(proxy.NewPerHost(
+			&dialerLogger{"direct", proxy.Direct},
+			&dialerLogger{"proxy", c.proxy},
+		), s, string(customAutoproxy))
 	})
+	if err := watchFile(
+		*custom,
+		func() {
+			c.autoproxy.Lock()
+			defer c.autoproxy.Unlock()
+			customAutoproxy, _ = os.ReadFile(*custom)
+			c.autoproxy.PerHost = parseAutoproxy(proxy.NewPerHost(
+				&dialerLogger{"direct", proxy.Direct},
+				&dialerLogger{"proxy", c.proxy},
+			), last, string(customAutoproxy))
+		},
+		func() {
+			c.autoproxy.Lock()
+			defer c.autoproxy.Unlock()
+			customAutoproxy = nil
+			c.autoproxy.PerHost = addPerHost(proxy.NewPerHost(
+				&dialerLogger{"direct", proxy.Direct},
+				&dialerLogger{"proxy", c.proxy},
+			), last, false)
+		},
+	); err != nil {
+		errorLogger.Print(err)
+	}
 	return p
 }
 
